a24795c5b5b3485a3ce9d527f717fd5d748b1186
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-06-14 11:40:19 +0800

    Added test for atoms

---
+++ tests/atoms_test.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include"atoms.hpp"
+#include<boost/shared_ptr.hpp>
+#include<boost/scoped_ptr.hpp>
+
+boost::shared_ptr<Atom> NILATOM;
+
+boost::scoped_ptr<Globals> globals;
+
+int main(int argc, const char* argv[]){
+	globals.reset(new Globals());
+	NILATOM = globals->lookup("nil");
+
+	/*tests!*/
+	/*make sure NILATOM is a global atom*/
+	GlobalAtom* p = dynamic_cast<GlobalAtom*>(&*NILATOM);
+	if(p != NULL){
+		std::cout << "NIL ATOM: ";
+		p->emit();
+		std::cout << std::endl;
+	} else {
+		std::cout << "NIL ATOM not global!" << std::endl;
+	}
+	/*make sure NILATOM is looked up again*/
+	boost::shared_ptr<Atom> tmp(globals->lookup("nil"));
+	if(tmp != NILATOM){
+		std::cout << "Eh??" << std::endl;
+		p = dynamic_cast<GlobalAtom*>(&*tmp);
+		if(p != NULL){
+			std::cout << "Other 'nil atom: ";
+			p->emit();
+			std::cout << std::endl;
+		} else {
+			std::cout << "Other 'nil atom isn't even global!"
+				<< std::endl;
+		}
+	} else {
+		std::cout << "Got NILATOM on lookup again!" << std::endl;
+	}
+}
+
