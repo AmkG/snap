161b616346c072e44a78032d81974756c433d5e8
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-06-15 22:22:18 +0800

    Added a little more allocation

--- tests/heaps_test.cpp
+++ tests/heaps_test.cpp
@@ -11,16 +11,27 @@ int main(int argc, const char* argv[]){
 	/*tests!*/
 	Process proc;
 	/*allocate stuff*/
+	//(cons t ...)
+	proc.stack.push(new(proc) Sym(TATOM));//t
 	//(cons t nil)
 	proc.stack.push(new(proc) Sym(TATOM)); //t
 	proc.stack.push(new(proc) Sym(NILATOM)); //nil
-	std::cout << proc.stack.size() << std::endl;
 	//cons
-	Cons* cp = new(proc) Cons();
-	cp->a = proc.stack.top(2);
-	cp->d = proc.stack.top();
-	proc.stack.top(2) = cp;
-	proc.stack.pop();
+	{
+		Cons* cp = new(proc) Cons();
+		cp->a = proc.stack.top(2);
+		cp->d = proc.stack.top();
+		proc.stack.top(2) = cp;
+		proc.stack.pop();
+	}
+	//another cons
+	{
+		Cons* cp = new(proc) Cons();
+		cp->a = proc.stack.top(2);
+		cp->d = proc.stack.top();
+		proc.stack.top(2) = cp;
+		proc.stack.pop();
+	}
 
 	proc.stack.top()->probe(0);
 }
