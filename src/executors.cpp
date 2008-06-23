db92ae780f60ee075fd437d3303d33008c0346b6
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-06-23 20:48:56 +0800

    Started interpreter.
      An Executor is the core of the Arc function.
      In the Arc-side, the function is a closure which includes enclosed
        variables.
      In the C++-side, the closure contains an Executor which represents
        the function to be called

---
+++ src/executors.cpp
@@ -0,0 +1,33 @@
+#include"variables.hpp"
+#include"executors.hpp"
+#include"atoms.hpp"
+#include"processes.hpp"
+
+ProcessStatus execute(Process& proc, bool init=0){
+	if(init) goto initialize;
+	DISPATCH_EXECUTORS {
+		EXECUTOR(arc_executor):
+		NEXT_EXECUTOR;
+		/*
+		(fn (k#|1|# f#|2|#)
+		  (f k (fn (_ r) (k r))))
+		*/
+		EXECUTOR(ccc):
+		{	Closure* c = new(proc)
+				Closure(THE_EXECUTOR(ccc_helper), 1);
+			(*c)[0] = proc.stack[1/*k*/];
+			proc.stack[0] = proc.stack[2/*f*/];
+			proc.stack[2] = c;
+		} NEXT_EXECUTOR;
+		EXECUTOR(ccc_helper):
+		{	Closure* c = dynamic_cast<Closure*>(proc.stack[0]);
+			proc.stack[0] = (*c)[0/*k*/];
+			proc.stack[1] = proc.stack[2/*r*/];
+			proc.stack.pop();
+		} NEXT_EXECUTOR;
+	}
+	return dead;
+initialize:
+	return running;
+}
+
