e3e15332a8aae75c0e00fd7607a546c07fb29024
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-06-26 22:11:23 +0800

    Expanded tests slightly

--- tests/heaps_test.cpp
+++ tests/heaps_test.cpp
@@ -1,5 +1,6 @@
 #include"variables.hpp"
 #include"processes.hpp"
+#include"bytecodes.hpp"
 #include<iostream>
 
 int main(int argc, const char* argv[]){
@@ -8,27 +9,22 @@ int main(int argc, const char* argv[]){
 	/*tests!*/
 	Process proc;
 	/*allocate stuff*/
+	//(cons 'bar ...)
+	proc.stack.push(new(proc) Sym(globals->lookup("bar")));
+	//(cons 'foo ...)
+	proc.stack.push(new(proc) Sym(globals->lookup("foo")));
 	//(cons t ...)
 	proc.stack.push(new(proc) Sym(TATOM));//t
 	//(cons t nil)
 	proc.stack.push(new(proc) Sym(TATOM)); //t
 	proc.stack.push(new(proc) Sym(NILATOM)); //nil
 	//cons
-	{
-		Cons* cp = new(proc) Cons();
-		cp->a = proc.stack.top(2);
-		cp->d = proc.stack.top();
-		proc.stack.top(2) = cp;
-		proc.stack.pop();
-	}
-	//another cons
-	{
-		Cons* cp = new(proc) Cons();
-		cp->a = proc.stack.top(2);
-		cp->d = proc.stack.top();
-		proc.stack.top(2) = cp;
-		proc.stack.pop();
-	}
+	bytecode_cons(proc);
+	//repush stack top to make shared structure
+	proc.stack.push(proc.stack.top());
+	bytecode_cons(proc);
+	bytecode_cons(proc);
+	bytecode_cons(proc);
 
 	proc.stack.top()->probe(0);
 }
