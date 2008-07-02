99774d55e87631bd2aed401f5fccb3c4d219bd7d
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-07-02 13:50:54 +0800

    Added a few bytecodes, created a reference to the process stack, stack

--- src/executors.cpp
+++ src/executors.cpp
@@ -32,10 +32,28 @@ static _bytecode_label bytecodelookup(boost::shared_ptr<Atom> a){
 }
 
 ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
+	ProcessStack& stack = proc.stack;
 	if(init) goto initialize;
 	DISPATCH_EXECUTORS {
 		EXECUTOR(arc_executor):
 		{	DISPATCH_BYTECODES{//provides the Closure* clos
+				BYTECODE(apply):
+				{INTPARAM(N);
+					stack.restack(N);
+				} /***/ NEXT_EXECUTOR; /***/
+				BYTECODE(apply_list):
+				{NOPARAM;
+					Generic* tmp;
+					stack.restack(3);
+					/*destructure until stack top is nil*/
+					while(stack.top()->istrue()){
+						tmp = stack.top();
+						bytecode_car();
+						stack.push(tmp);
+						bytecode_cdr();
+					}
+					stack.pop();
+				} /***/ NEXT_EXECUTOR; /***/
 				BYTECODE(car):
 					bytecode_car(proc);
 				NEXT_BYTECODE;
@@ -54,15 +72,15 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 		EXECUTOR(ccc):
 		{	Closure* c = new(proc)
 				Closure(THE_EXECUTOR(ccc_helper), 1);
-			(*c)[0] = proc.stack[1/*k*/];
-			proc.stack[0] = proc.stack[2/*f*/];
-			proc.stack[2] = c;
+			(*c)[0] = stack[1/*k*/];
+			stack[0] = stack[2/*f*/];
+			stack[2] = c;
 		} NEXT_EXECUTOR;
 		EXECUTOR(ccc_helper):
-		{	Closure* c = expect_type<Closure>(proc.stack[0]);
-			proc.stack[0] = (*c)[0/*k*/];
-			proc.stack[1] = proc.stack[2/*r*/];
-			proc.stack.pop();
+		{	Closure* c = expect_type<Closure>(stack[0]);
+			stack[0] = (*c)[0/*k*/];
+			stack[1] = stack[2/*r*/];
+			stack.pop();
 		} NEXT_EXECUTOR;
 		/*(fn (k l)
 		    (compile_helper k (%empty-bytecode-sequence) l))
@@ -72,15 +90,15 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 				Closure(THE_EXECUTOR(compile_helper), 0);
 			ArcBytecodeSequence* bseq = new(proc)
 				ArcBytecodeSequence();
-			proc.stack[0] = c;
+			stack[0] = c;
 			/*just do type checking*/
-			if(proc.stack[1]->istrue()){
-				Cons* cp = expect_type<Cons>(proc.stack[1],
+			if(stack[1]->istrue()){
+				Cons* cp = expect_type<Cons>(stack[1],
 						"compile",
 						"Expected bytecode list");
 			}
-			proc.stack.push(proc.stack[2]);
-			proc.stack[2] = bseq;
+			stack.push(stack[2]);
+			stack[2] = bseq;
 		} NEXT_EXECUTOR;
 		/* (fn (self k b l)
 		     (if (no l)
@@ -109,76 +127,76 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 		EXECUTOR(compile_helper):
 		{	ArcBytecodeSequence* b =
 				dynamic_cast<ArcBytecodeSequence*>(
-					proc.stack[2]);
+					stack[2]);
 		compile_helper_loop:
 			/*check l (local 3)*/
-			if(proc.stack[3]->isnil()){
+			if(stack[3]->isnil()){
 				/*(k b)*/
-				proc.stack.pop();
-				proc.stack.restack(2);
+				stack.pop();
+				stack.restack(2);
 			} else {
 				/*split l into c and l*/
-				Cons* l = expect_type<Cons>(proc.stack[3],
+				Cons* l = expect_type<Cons>(stack[3],
 					"compile",
 					"Expected bytecode list in "
 					"compile_helper");
 				/*c == (local 4)*/
-				proc.stack.push(l->a);
-				proc.stack[3] = l->d;
+				stack.push(l->a);
+				stack[3] = l->d;
 				/*determine if c is cons*/
-				Cons* c = dynamic_cast<Cons*>(proc.stack[4]);
+				Cons* c = dynamic_cast<Cons*>(stack[4]);
 				if(c == NULL){/*(~acons c)*/
 					/*Get the atom*/
 					Sym* c = expect_type<Sym>(
-						proc.stack[4],
+						stack[4],
 						"compile",
 						"Expected bytecode symbol");
 					/*(%bytecode-sequence-append ...)*/
 					b->append(new Bytecode(
 							bytecodelookup(c->a)));
 					/*Just pop off c*/
-					proc.stack.pop();
+					stack.pop();
 					if(--reductions) goto compile_helper_loop; else return process_running;
 				} else {
 					/*destructure form*/
-					proc.stack[4] = c->a;
+					stack[4] = c->a;
 					Cons* params = expect_type<Cons>(c->d,
 						"compile",
 						"Expected proper list for "
 						"bytecode with parameter");
 					/*param = local 5*/
-					proc.stack.push(params->a);
+					stack.push(params->a);
 					/*params = local 6*/
-					proc.stack.push(params->d);
+					stack.push(params->d);
 					Integer* param =
 						dynamic_cast<Integer*>(
 							params->a);
 					if(param != NULL){/*(isa param 'int)*/
 						/*check if params is null*/
-						if(proc.stack[6]->istrue()){
+						if(stack[6]->istrue()){
 							/*IntSeq*/
 							Closure* clos = new(proc) Closure(THE_EXECUTOR(compile_intseq_bytecode), 6);
 							for(int i = 0; i < 6; ++i){
-								(*clos)[i] = proc.stack[i];
+								(*clos)[i] = stack[i];
 							}
 							/*call new continuation*/
-							proc.stack[3] = proc.stack[0];
-							proc.stack[4] = clos; // have to save this first!
-							proc.stack[5] = b = new(proc) ArcBytecodeSequence(); // make sure to reassign b
-							proc.stack.restack(4);
+							stack[3] = stack[0];
+							stack[4] = clos; // have to save this first!
+							stack[5] = b = new(proc) ArcBytecodeSequence(); // make sure to reassign b
+							stack.restack(4);
 							if(--reductions) goto compile_helper_loop; else return process_running;
 						} else {
 							/*Int*/
 							/*Get the atom*/
 							Sym* c = expect_type<Sym>(
-								proc.stack[4],
+								stack[4],
 								"compile",
 								"Expected bytecode symbol "
 								"in int-type bytecode form");
 							/*(%bytecode-sequence-append ...)*/
 							b->append(new IntBytecode(bytecodelookup(c->a), param->integer()));
 							/*pop off c, param, and params*/
-							proc.stack.pop(3);
+							stack.pop(3);
 							if(--reductions) goto compile_helper_loop; else return process_running;
 						}
 					} else {
@@ -189,7 +207,7 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 								"Expected bytecode symbol or quote in parameter to bytecode");
 							if(carparam->a == QUOTEATOM){
 								Sym* c = expect_type<Sym>(
-									proc.stack[4],
+									stack[4],
 									"compile",
 									"Expected bytecode symbol "
 									"in symbol-parameter bytecode form");
@@ -201,7 +219,7 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 									"Expected symbol in symbol-parameter bytecode");
 								b->append(new AtomBytecode(bytecodelookup(c->a), carparam->a));
 								/*pop off c, param, and params*/
-								proc.stack.pop(3);
+								stack.pop(3);
 								if(--reductions)goto compile_helper_loop; else return process_running;
 							} else goto compile_helper_non_quote_param;
 						}
@@ -217,15 +235,15 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 						we don't alloc anything along the code path to
 						this part, so we won't trigger GC.
 						*/
-						proc.stack[5] = params;
-						proc.stack.pop();
+						stack[5] = params;
+						stack.pop();
 						Closure* clos = new(proc) Closure(THE_EXECUTOR(compile_seq_bytecode), 5);
 						for(int i = 0; i < 5; ++i){
-							(*clos)[i] = proc.stack[i];
+							(*clos)[i] = stack[i];
 						}
-						proc.stack[1] = clos; // Have to save this first!
-						proc.stack[2] = b = new(proc) ArcBytecodeSequence(); // make sure to reassign b
-						proc.stack[3] = proc.stack.top(); proc.stack.pop();
+						stack[1] = clos; // Have to save this first!
+						stack[2] = b = new(proc) ArcBytecodeSequence(); // make sure to reassign b
+						stack[3] = stack.top(); stack.pop();
 						if(--reductions) goto compile_helper_loop; else return process_running;
 					}
 				}
@@ -243,9 +261,9 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 		*/
 		EXECUTOR(compile_intseq_bytecode):
 		{	Closure* clos =
-				dynamic_cast<Closure*>(proc.stack[0]);
+				dynamic_cast<Closure*>(stack[0]);
 			ArcBytecodeSequence* subseq =
-				dynamic_cast<ArcBytecodeSequence*>(proc.stack[1]);
+				dynamic_cast<ArcBytecodeSequence*>(stack[1]);
 			/*(let ...)*/
 			ArcBytecodeSequence* b =
 				dynamic_cast<ArcBytecodeSequence*>(
@@ -261,9 +279,9 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 			b->append(new IntSeqBytecode(bytecodelookup(c->a), param->integer(), subseq->seq));
 			/*push the closure elements*/
 			for(int i = 0; i < 4; ++i){
-				proc.stack.push((*clos)[i]);
+				stack.push((*clos)[i]);
 			}
-			proc.stack.restack(4);
+			stack.restack(4);
 		} NEXT_EXECUTOR;
 		/*
 		(fn (clos subseq)
@@ -275,9 +293,9 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 		*/
 		EXECUTOR(compile_seq_bytecode):
 		{	Closure* clos =
-				dynamic_cast<Closure*>(proc.stack[0]);
+				dynamic_cast<Closure*>(stack[0]);
 			ArcBytecodeSequence* subseq =
-				dynamic_cast<ArcBytecodeSequence*>(proc.stack[1]);
+				dynamic_cast<ArcBytecodeSequence*>(stack[1]);
 			/*(let ...)*/
 			ArcBytecodeSequence* b =
 				dynamic_cast<ArcBytecodeSequence*>(
@@ -290,9 +308,9 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 			b->append(new SeqBytecode(bytecodelookup(c->a), subseq->seq));
 			/*push the closure elements*/
 			for(int i = 0; i < 4; ++i){
-				proc.stack.push((*clos)[i]);
+				stack.push((*clos)[i]);
 			}
-			proc.stack.restack(4);
+			stack.restack(4);
 		} NEXT_EXECUTOR;
 		/*used to create a free function (no enclosed variables)
 		from a bytecode sequence
@@ -301,15 +319,15 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 		*/
 		EXECUTOR(to_free_fun):
 		{	ArcBytecodeSequence* b =
-				expect_type<ArcBytecodeSequence>(proc.stack.top());
+				expect_type<ArcBytecodeSequence>(stack.top());
 			Closure* clos =
 				new(proc) Closure(THE_ARC_EXECUTOR(arc_executor, b->seq), 0);
-			proc.stack[2] = clos;
-			proc.stack.restack(2);
+			stack[2] = clos;
+			stack.restack(2);
 		} NEXT_EXECUTOR;
 		EXECUTOR(halting_continuation):
-			proc.stack.push(proc.stack[1]);
-			proc.stack.restack(1);
+			stack.push(stack[1]);
+			stack.restack(1);
 			return process_dead;
 		NEXT_EXECUTOR;
 		/* this function implements the ($ ...) dispatcher
@@ -321,30 +339,30 @@ ProcessStatus execute(Process& proc, size_t reductions, bool init=0){
 		    ...))
 		*/
 		EXECUTOR(bif_dispatch):
-		{	Sym* s = expect_type<Sym>(proc.stack[2],
+		{	Sym* s = expect_type<Sym>(stack[2],
 					"$",
 					"Expected a symbol");
 			/*if-else-if chain*/
 			if(s->a == CCCATOM){
-				proc.stack[2] =
+				stack[2] =
 					new(proc)
 					Closure(THE_EXECUTOR(ccc), 0);
 			} else if(s->a == BYTECODERATOM){
-				proc.stack[2] =
+				stack[2] =
 					new(proc)
 					Closure(THE_EXECUTOR(compile), 0);
 			} else if(s->a == FREEFUNATOM){
-				proc.stack[2] =
+				stack[2] =
 					new(proc)
 					Closure(THE_EXECUTOR(to_free_fun), 0);
 			} else if(s->a == HALTATOM){
-				proc.stack[2] =
+				stack[2] =
 					new(proc)
 					Closure(THE_EXECUTOR(halting_continuation), 0);
 			} else {
-				proc.stack[2] = new(proc) Sym(NILATOM);
+				stack[2] = new(proc) Sym(NILATOM);
 			}
-			proc.stack.restack(2);
+			stack.restack(2);
 		} NEXT_EXECUTOR;
 	}
 	return process_dead;
