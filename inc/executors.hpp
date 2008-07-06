9955aa5f0d5573a0f335be1babcbdb1ad7549c48
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-07-06 10:20:48 +0800

    executors.hpp: Modified comments

--- inc/executors.hpp
+++ inc/executors.hpp
@@ -10,15 +10,12 @@ attached Executor object.
 /*
 Calling convention:
 The Executor is given a Process which is the process within
-which it must execute itself.  Assume you have:
-	virtual void YourExecutor::operator()(Process& proc,
-					Closure& clos) const{
-		...
-	}
-proc.stack[0] contains the closure (the same as clos).  If
-the Executor represents a "normal" function, proc.stack[1]
-is the "continuation" function and proc.stack[2..n] are
-the parameters.  proc.stack.size() is the number of
+which it must execute itself.
+
+proc.stack[0] contains the closure.  If the Executor
+represents a "normal" function, proc.stack[1] is the
+"continuation" function and proc.stack[2..n] are the
+parameters.  proc.stack.size() is the number of
 parameters including the function itself and the
 continuation.
 
