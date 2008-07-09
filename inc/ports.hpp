340a6d7e382cf012e5aa8de0956157daa4a72c2f
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-07-09 19:45:46 +0800

    ports: started asynchronous I/O

---
+++ inc/ports.hpp
@@ -0,0 +1,92 @@
+#ifndef PORTS_H
+#define PORTS_H
+#include<vector>
+#include<boost/scoped_ptr.hpp>
+#include<boost/shared_ptr.hpp>
+#include<string>
+
+/*abstract class*/
+class AsyncPort{
+public:
+	// returns true if input is available on the port
+	virtual bool input_available(void) const =0;
+	// size of vector denotes desired number of characters
+	//  (call vector::resize() before calling this function)
+	// upon return, vector now contains the actual read
+	//  number of bytes
+	virtual void input(std::vector<unsigned char>&) =0;
+	virtual bool output_available(void) const =0;
+	virtual void output(std::vector<unsigned char> const&) =0;
+	virtual bool is_input(void) const =0;
+	virtual bool is_output(void) const =0;
+};
+
+AsyncPort* AsyncSTDIN(void);
+AsyncPort* AsyncSTDOUT(void);
+AsyncPort* AsyncSTDERR(void);
+
+#define DECLARE_ASYNC_PORT\
+	virtual bool input_available(void) const;\
+	virtual void input(std::vector<unsigned char>&);\
+	virtual bool output_available(void) const;\
+	virtual void output(std::vector<unsigned char> const&);\
+	virtual bool is_input(void) const;\
+	virtual bool is_output(void) const;\
+	friend AsyncPort* AsyncSTDIN(void);\
+	friend AsyncPort* AsyncSTDOUT(void);\
+	friend AsyncPort* AsyncSTDERR(void);
+/*include above as friends, we can't be too sure that
+those functions might, in some random implementation,
+actually construct a derived concrete class.
+*/
+
+/*---------------------------------------------------------concrete classes*/
+
+class InputFileImplementation;
+
+class InputFilePort : public AsyncPort{
+private:
+	//Pointer to implementation
+	boost::scoped_ptr<InputFileImplementation> p;
+	InputFilePort();
+public:
+	DECLARE_ASYNC_PORT
+	InputFilePort(std::string const&);
+};
+
+class OutputFileImplementation;
+
+class OutputFilePort : public AsyncPort{
+private:
+	//Pointer to implementation
+	boost::scoped_ptr<OutputFileImplementation> p;
+	OutputFilePort();
+public:
+	DECLARE_ASYNC_PORT
+	OutputFilePort(std::string const&);
+}
+
+/*question: can we do terminal-based i/o that way or no?*/
+
+/*-------------------------------------------sets of AsyncPorts for waiting*/
+
+class AsyncPortSetImplementation
+
+class AsyncPortSet{
+private:
+	boost::scoped_ptr<AsyncPortSetImplementation> p;
+public:
+	void add(boost::shared_ptr<AsyncPort>);
+	void remove(boost::shared_ptr<AsyncPort>);
+	//nonblocking
+	// returns all asynchronous ports that have data
+	// waiting.  those ports are automatically removed
+	// from this set.
+	std::vector<boost::shared_ptr<AsyncPort> > check(void);
+	//blocking (if we detect that we're the only running
+	//process)
+	std::vector<boost::shared_ptr<AsyncPort> > wait(void);
+};
+
+#endif //PORTS_H
+
