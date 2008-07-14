316796bdbf94be49cea3b64426d5170550b62e55
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-07-14 20:54:21 +0800

    runqueues: added missing class namespace qualifier

--- src/runqueues.cpp
+++ src/runqueues.cpp
@@ -7,7 +7,7 @@
 #include<queue>
 
 /*NOTE!  Better to use lock-free implementation if available!*/
-void pop(boost::shared_ptr<ProcessBase>& rv){
+void RunQueue::pop(boost::shared_ptr<ProcessBase>& rv){
 	/*insert locking of runqueue here*/
 	if(Q.empty()){
 		rv.reset(); return;
@@ -16,7 +16,7 @@ void pop(boost::shared_ptr<ProcessBase>& rv){
 	Q.pop();
 }
 
-void pop_push(boost::shared_ptr<ProcessBase>& rv){
+void RunQueue::pop_push(boost::shared_ptr<ProcessBase>& rv){
 	/*insert locking of runqueue here*/
 	if(Q.empty()) return; // don't bother pushing if empty
 	Q.push(rv);
@@ -24,7 +24,7 @@ void pop_push(boost::shared_ptr<ProcessBase>& rv){
 	Q.pop();
 }
 
-void push(boost::shared_ptr<ProcessBase> const& v){
+void RunQueue::push(boost::shared_ptr<ProcessBase> const& v){
 	/*insert locking of runqueue here*/
 	Q.push(v);
 }
